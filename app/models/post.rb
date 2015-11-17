29ef6d4a73d41f49449bf2860e4e05fb17f19752
Andrew Trout <andrewrtrout@gmail.com>
2015-11-17 12:28:13 -0500

    Re-ordered posts, changed some styling, and allowed text only posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base  
-  validates :image, presence: true
+  validates :caption, presence: true, unless: ->(post){image.present?}
 
   has_attached_file :image, styles: { :medium => "640x" }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
